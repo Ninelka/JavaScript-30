177eb1eec5ac7c7e9ef7be47284eb9533c8d87da
Ninel <ninelka66@gmail.com>
2019-03-28 01:45:27 +0500

    Extra changes

--- CSS+JS Clock/index.js
+++ CSS+JS Clock/index.js
@@ -2,6 +2,8 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+const allHands = document.querySelectorAll('.hand');
+
 
 function setDate() {
     const now = new Date();
@@ -17,6 +19,12 @@ function setDate() {
     const hour = now.getHours();
     const hourDegrees = ((hour / 60) * 360 ) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    if(secondsDegrees === 90) {
+        allHands.forEach(hand => hand.style.transition = 'none')
+    } else {
+        allHands.forEach(hand => hand.style.transition = '')      // Используя '' удаляем инлайновый стиль
+    }
 }
 
 setInterval(setDate, 1000);
